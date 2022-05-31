92929916667c3f12b4832bc002f13b69372c2afa
Forestieur <Forestieur@gmail.com>
2022-05-31 06:41:57 +0300

    final changes

--- bin/Lab1.dart
+++ bin/Lab1.dart
@@ -6,10 +6,6 @@ void main() {
   int? boardSize = 8;
   var board = chess_board.ChessBoard(boardSize);
   board.Solve();
-  print(board.count);
-
-  print("length is ${board.solutions.length}");
-
   for (int i = 0; i < board.count; i++) {
     print("solution number ${i + 1}");
     for (int row = 0; row < boardSize; row++) {
